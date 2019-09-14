0df19d14b5df0ca2c9b9a8faf8c6446a0023fca4
Chuck Dries <chuck@chuckdries.com>
2019-09-14 00:50:31 -0700

    Add web server with basic routing (query & params), function examples

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-console.log("hello world");
-let planets = ["sun", "earth", "venus", "mercury"];
-planets = ["asdf"];
+const express = require("express");
+const app = express();
 
-for (planet of planets) {
-  console.log(`I want to go to ${planet}`);
-}
+// // examples of function definitions
+// function add(x, y) {
+//   return x + y;
+// }
+
+// const subtract = function(x, y) {
+//   return x - y;
+// };
+
+// const multiply = (x, y) => {
+//   return x * y;
+// };
+
+// const divide = (x, y) => x / y;
+
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>");
+});
+
+app.get("/greet", (req, res) => {
+  const name = req.query.name;
+  console.log(req.query);
+  res.send(`<h1>hello via query, ${name}!`);
+});
+
+app.get("/greet/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`<h1>hello via param, ${name}!`);
+});
+
+app.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
