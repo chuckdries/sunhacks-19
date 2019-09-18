e2848d0b1923215418533cc91f385c98523c2e8e
Chuck Dries <chuckd@axosoft.com>
2019-09-17 21:50:18 -0700

    Add greeting routes that take names as parameters

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/greet/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello ${name}! Welcome to your page`);
+});
+
+app.get("/greet", (req, res) => {
+  const name = req.query.name;
+  if (name) {
+    res.send(`Hello ${name}!`);
+  } else {
+    res.send("Well this is awkward... I don't know who you are");
+  }
+});
+
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
 });
