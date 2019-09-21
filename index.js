4f5002e8d49e0733cd8492ef296afbf4ac3c77f2
Chuck Dries <chuckd@axosoft.com>
2019-09-20 22:04:05 -0700

    Implement database based message storage and retrieval

--- index.js
+++ index.js
@@ -2,21 +2,9 @@
 import express from "express";
 import hbs from "express-handlebars";
 import bodyParser from "body-parser";
+import sqlite from "sqlite";
 
-import { getPromiseTimer } from "./utilities/promiseTimer";
-
-getPromiseTimer(300, "howdy").then(value => {
-  console.log("used .then: " + value);
-});
-
-const asyncTest = async () => {
-  const yoTimer = getPromiseTimer(1000, "yo");
-  const greeting = await yoTimer;
-  console.log("used await: " + greeting);
-};
-
-asyncTest();
-
+const dbPromise = sqlite.open("./database.sqlite");
 const app = express();
 
 app.engine("handlebars", hbs());
@@ -25,15 +13,23 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
 
-const messages = [];
-
-app.get("/", (req, res) => {
-  res.render("home", { messages: messages });
+dbPromise.then(db => {
+  db.run(`CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY,
+    message STRING
+  )`);
 });
 
-app.post("/message", (req, res) => {
-  const message = req.body.message;
-  messages.push(message);
+app.get("/", async (req, res) => {
+  const db = await dbPromise;
+  const messages = await db.all("SELECT * FROM messages");
+  res.render("home", { messages });
+});
+
+app.post("/message", async (req, res) => {
+  const db = await dbPromise;
+  const { message } = req.body;
+  await db.run("INSERT INTO messages (message) VALUES (?)", message);
   res.redirect("/");
 });
 
