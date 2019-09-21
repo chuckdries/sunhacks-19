3721e9fa348f0dbaeb0dd2b675a27e865d2bed1c
Chuck Dries <chuckd@axosoft.com>
2019-09-21 04:50:40 -0700

    Database message board

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const sqlite = require("sqlite");
 
+const dbPromise = sqlite.open("./database.sqlite");
 const app = express();
 
 app.engine("handlebars", exphbs());
@@ -10,21 +12,36 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static("./public"));
 
-const messages = [];
 // index route
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const db = await dbPromise;
+  const messages = await db.all("SELECT * FROM messages");
   res.render("home", { messages }); // { messages: messages }
 });
 
-app.post("/message", (req, res) => {
+app.post("/message", async (req, res) => {
   const message = req.body.message;
-  messages.push(message);
+  const db = await dbPromise;
+  await db.run("INSERT INTO messages (message) VALUES (?)", message);
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("listening on http://localhost:3000");
-});
+const setup = async () => {
+  const db = await dbPromise;
+  await db.run(`CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY,
+    message STRING
+  );`);
+  app.listen(3000, () => {
+    console.log("listening on http://localhost:3000");
+  });
+};
+
+setup();
+
+// app.listen(3000, function() {
+//   console.log("listening on http://localhost:3000");
+// });
 
 // () => {}
 // function() {}
