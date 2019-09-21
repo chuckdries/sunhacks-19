6e6bf901c29c52ac6caed216da7ba5bbfe6cf431
Chuck Dries <chuckd@axosoft.com>
2019-09-21 04:25:46 -0700

    in memory message board

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static("./public"));
 
+const messages = [];
 // index route
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { messages }); // { messages: messages }
 });
 
-app.get("/greet", (req, res) => {
-  const name = req.query.name;
-  res.render("greet", { name: name });
+app.post("/message", (req, res) => {
+  const message = req.body.message;
+  messages.push(message);
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
